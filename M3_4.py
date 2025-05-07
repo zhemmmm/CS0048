6f10659926d9987ae6e6300ea046d0fae48ed151
Zhemier Padora <zhemierpadora@gmail.com>
2025-05-07 12:38:49 +0800

    Update M3_4.py

--- M3_4.py
+++ M3_4.py
@@ -1,40 +1,45 @@
 import random
 
+print("\nNumber Guessing Game")
+
 def menu():
-    print("\nMenu")
-    print("1. Play Number Guessing Game")
-    print("2. Exit")
+    print("\nMenu:")
+    print(" 1. Play Number Guessing Game")
+    print(" 2. Exit")
 
-    choice_str = input("Enter your choice (1-2): ")
+def play_game():
+    guess_num = random.randint(1, 100)
+    attempts = 0
 
-while True:
-    choice_str = menu()
+    while True:
+        guess = input(" Guess the number (1-100): ")
 
-    if not choice_str.isdigit():
-        print("Invalid input. Please enter a number between 1 and 2.")
-        continue
+        if not guess.isdigit():
+            print("Invalid input. Choose a number from 1-100")
+            continue
 
-    choice = int(choice_str)
+        guess = int(guess)
+        attempts += 1
 
-    if choice == 2:
-        print("Exiting the program.")
-        break
-    elif choice == 1:
-        number = random.randint(1, 100)
-        attempts = 0
-        while True:
-            
-            guess_str = input("Guess a number between 1 and 100: ")
-            guess = int(guess_str)
-            attempts += 1
+        if guess < guess_num:
+            print(" Too low!")
+        elif guess > guess_num:
+            print(" Too high!")
+        else:
+            print(f"Congratulations! You guessed the number in {attempts} attempts.")
+            break
 
-            if guess < number:
-                print("Too low!")
-            elif guess > number:
-                print("Too high!")
-            else:
-                print(f"Correct! You guessed the number in {attempts} attempts.")
-                break
-        
-    else:
-        print("Invalid choice. Please enter a number between 1 and 2.")
+def main():
+    while True:
+        menu()
+        choice = input("Enter your choice (1-2): ")
+
+        if choice == '1':
+            play_game()
+        elif choice == '2':
+            print("Thank you for using the number guessing game")
+            break
+        else:
+            print("Invalid choice. Choose from (1-2) pls.")
+
+main()
